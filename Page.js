89f7897c7e18ea4e3649342fdb0046fd7f82eae5
hemnaa08 <hemnaa326@gmail.com>
2024-09-17 12:25:09 +0530

    ALL SET

--- Page.js
+++ Page.js
@@ -1,18 +1,27 @@
 function showPage(pageNumber, event) {
-    // Prevent the default behavior of the anchor tag (scrolling to the top)
     if (event) {
         event.preventDefault();
     }
 
-    // Hide all pages
+    
     document.getElementById("page-1").style.display = "none";
     document.getElementById("page-2").style.display = "none";
     document.getElementById("page-3").style.display = "none";
-
-    // Show the selected page
     document.getElementById("page-" + pageNumber).style.display = "flex";
 
-    // Scroll back to the "Indoor" section smoothly
-    document.getElementById("indoor").scrollIntoView({ behavior: "smooth" });
+    
+}
+function showOutdoorPage(pageNumber, event) {
+    if (event) {
+        event.preventDefault();
+    }
+
+    
+    document.getElementById("outdoor-page-1").style.display = "none";
+    document.getElementById("outdoor-page-2").style.display = "none";
+    document.getElementById("outdoor-page-3").style.display = "none";
+
+    document.getElementById("outdoor-page-" + pageNumber).style.display = "flex";
+
 }
 
